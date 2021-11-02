a7cf85b0b07b30ee7e0753d383051b7d69be7024
EmilyDSarani <emily.sarani@gmail.com>
2021-11-02 11:26:07 -0500

    pseudo coded a plan for each of the pages

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 7890; //this is basically saying "if there is a env.PORT use that or use the 7890 if you cant find it"
 
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`🚀  Server running on port http://localhost:${PORT}`);
+const server = http.createServer(app); //this is creating the server
+ 
+server.listen(PORT, () => {  //the server is listening for which port to launch on
+  console.log(`Server running on port http://localhost:${PORT}`); //this is just a console log to tell us which port the server is running on
 });
