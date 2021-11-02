cee479abccd2dc8cadc22a71693626e814fa04f1
EmilyDSarani <emily.sarani@gmail.com>
2021-11-02 13:26:24 -0500

    red post test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,11 +1,32 @@
 //should probably write each of these tests first
+//beforeEach, and afterAll- deletes and creates fresh store folders. This is so when we do our getAll, we are getting the EXACT number of items we expect instead of a ton of different things that were created. 
+//this is like the setup crew
+const request = require('supertest');
+const { rm, mkdir } = require('fs/promises');
+const app = require('../lib/app');
+const SimpleDb = require('../lib/simple-db');
 
-//Post Test
 
-//Get Id Test
+const rootDir = `${__dirname}/store`;
 
-//Get All Test
+describe('singer CRUD API', () => {
+  beforeEach(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
+  
+  afterAll(() => { 
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
 
-//Delete Test
+  it('POST creating the new singer and returning it', async () => {
 
-//Put Test 
+    const singer = { name: 'Brandon Urie', age: 34, genre: 'pop punk' };
+    const res = await request(app).post('/singers').send(singer);
+
+    expect(res.body).toEqual({ ...singer, id: expect.any(String) });
+  });
+});
